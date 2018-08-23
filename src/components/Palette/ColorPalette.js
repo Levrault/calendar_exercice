282e9718d29d287ffde2367547bdd2f079eb9445
Luc-Frédéric Langis <luc-frederic.langis@ekino.com>
2018-08-23 11:24:02 +0200

    s

--- src/components/Palette/ColorPalette.js
+++ src/components/Palette/ColorPalette.js
@@ -1,61 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classnames from 'classnames';
 import ColorButton from '../Button/ColorButton';
 import styles from './ColorPalette.css';
+import colors from './colors-constant';
 
-// https://material.io/design/color/the-color-system.html#tools-for-picking-colors
-const colors = [
-  {
-    code: '#FF5252',
-    name: 'red'
-  },
-  {
-    code: '#FF4081',
-    name: 'pink'
-  },
-  {
-    code: '#E040FB',
-    name: 'purple'
-  },
-  {
-    code: '#FF6E40',
-    name: 'orange'
-  },
-  {
-    code: '#448AFF',
-    name: 'blue'
-  },
-  {
-    code: '#64FFDA',
-    name: 'teal'
-  },
-  {
-    code: '#B2FF59',
-    name: 'green'
-  },
-  {
-    code: '#FFFF00',
-    name: 'yellow'
-  }, {
-    code: '#795548',
-    name: 'brown'
-  },
-  {
-    code: '#546E7A',
-    name: 'blueGray'
-  },
-  {
-    code: '#0097A7',
-    name: 'cyan'
-  },
-  {
-    code: '#FF6F00',
-    name: 'amber'
-  }
-];
 
-const ColorPalette = () => {
+/**
+ * Show color palette, change calender selected date's color
+ * @param {bool} active
+ */
+const ColorPalette = ({ active }) => {
+  const containerClassName = classnames(styles.colorPalette, {
+    [styles.active]: active
+  });
   return (
-    <ul className={styles.colorPalette}>
+    <ul className={containerClassName}>
       {
         colors.map(({ code, name }) => (
           <li key={name} className={styles.color} >
@@ -65,6 +25,14 @@ const ColorPalette = () => {
       }
     </ul>
   );
+};
+
+ColorPalette.propTypes = {
+  active: PropTypes.bool
+};
+
+ColorPalette.defaultProps = {
+  active: false
 };
 
 export default ColorPalette;
---
+++ src/components/Palette/colors-constant.js
@@ -0,0 +1,50 @@
+// https://material.io/design/color/the-color-system.html#tools-for-picking-colors
+export default [
+  {
+    code: '#FF5252',
+    name: 'red'
+  },
+  {
+    code: '#FF4081',
+    name: 'pink'
+  },
+  {
+    code: '#E040FB',
+    name: 'purple'
+  },
+  {
+    code: '#FF6E40',
+    name: 'orange'
+  },
+  {
+    code: '#448AFF',
+    name: 'blue'
+  },
+  {
+    code: '#64FFDA',
+    name: 'teal'
+  },
+  {
+    code: '#B2FF59',
+    name: 'green'
+  },
+  {
+    code: '#FFFF00',
+    name: 'yellow'
+  }, {
+    code: '#795548',
+    name: 'brown'
+  },
+  {
+    code: '#546E7A',
+    name: 'blueGray'
+  },
+  {
+    code: '#0097A7',
+    name: 'cyan'
+  },
+  {
+    code: '#FF6F00',
+    name: 'amber'
+  }
+];
